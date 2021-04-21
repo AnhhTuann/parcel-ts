8a91398293a0293b1cd1d2da631b44fe8b424538
AnhhTuann <tuannguyen25101999@gmail.com>
2021-04-21 23:40:47 +0700

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "../styles/style.scss";
+
+const App = (): JSX.Element => {
+  return <div className="tuan">asda</div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import App from "./components/App";
+const ROOT = document.querySelector(".container");
+
+ReactDom.render(<App />, ROOT);
